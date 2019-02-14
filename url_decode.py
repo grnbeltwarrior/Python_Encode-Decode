1cf41235a45ea7b7ab3737540d20ba0f1150ad77
Gabe Thompson <40601696+grnbeltwarrior@users.noreply.github.com>
2019-02-14 16:56:08 -0600

    Update url_decode.py

--- url_decode.py
+++ url_decode.py
@@ -4,8 +4,8 @@ import sys
 from urllib.parse import unquote
 
 if len(sys.argv) == 1:
-	print("No arguments passed. Feed me a set of ASCII numbers.")
-	print("Example: ./ascii2char.py '100 102 104 98'")
+	print("No arguments passed. Feed me a URL encoded string.")
+	print("Example: ./url_decode.py %63%70%61%73%62%69%65%6e%64%75%72%70%61%73%73%77%6f%72%64")
 	exit()
 
 else:
