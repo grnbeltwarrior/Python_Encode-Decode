7ebc7ce767e4d417185392ed3be0a38786e3b12f
Gabe Thompson <40601696+grnbeltwarrior@users.noreply.github.com>
2019-02-25 20:09:41 -0600

    Added function to be called by other scripts.

--- hex2text.py
+++ hex2text.py
@@ -3,11 +3,14 @@
 import sys
 import codecs
 
+def hex2text(hex):
+  temp = ''.join(hex.split())
+  result = codecs.decode(temp,'hex').decode('utf-8',errors='ignore')
+  return result
+
 if len(sys.argv) == 1:
   print("No arguments passed. Feed me a hex string.")
   exit()
 else:
         hex = sys.argv[1]
-        temp = ''.join(hex.split())
-        result = codecs.decode(temp,'hex').decode('utf-8',errors='ignore')
-        print("Original hex: ",hex,"\nConverted to text: ",result)
+        print("Original hex: ",hex,"\nConverted to text: ",hex2text(hex))
